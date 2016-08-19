fb28e49e6b3e41e63963e953bc1c960a0d114b6f
Dar Dahlen <dardahlen@gmail.com>
2016-08-19 14:17:32 -0700

    Added Setup File

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='TekAwg',
+      version='0.1',
+      description='Tektronix AWG ethernet interface for python',
+      author='Dar Dahlen',
+      author_email='dardahlen@gmail.com',
+      url='https://github.com/dahlend/TekAwg/',
+      packages=['numpy'],
+     )
